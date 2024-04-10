4ecc0fa3e11991dcc6226274673efd66db76ddfb
prahlad.singh-coditas <prahlad.singh@coditas.com>
2024-04-10 17:38:12 +0530

    Dependency Injection implemented

--- src/app/authentication/handlers/signInHandler.go
+++ src/app/authentication/handlers/signInHandler.go
@@ -11,21 +11,21 @@ import (
 
 // SignInHandler is a handler function for user sign-in endpoint.
 func SignInHandler(signInService *service.UserSignInService) gin.HandlerFunc {
-	return func(c *gin.Context) {
+	return func(context *gin.Context) {
 		var userCredentials models.SignInCredentials
 
 		// Binding the body of the incoming POST request
-		if err := c.ShouldBindJSON(&userCredentials); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": constants.ErrInBindingData})
+		if err := context.ShouldBindJSON(&userCredentials); err != nil {
+			context.JSON(http.StatusBadRequest, gin.H{"error": constants.ErrInBindingData})
 			return
 		}
 
 		// Call the SignInValidations method to validate user credentials
 		if err := signInService.SignInValidations(userCredentials); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid credentials, please try again"})
+			context.JSON(http.StatusBadRequest, gin.H{"error": "Invalid credentials, please try again"})
 			return
 		}
 
-		c.JSON(http.StatusOK, gin.H{"message": "Authentication successful"})
+		context.JSON(http.StatusOK, gin.H{"message": "Authentication successful"})
 	}
 }
